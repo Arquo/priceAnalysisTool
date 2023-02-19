a68e6dd7fc66c06b7ea94898db6beba98e41059c
Ruikang Chen <51214295+Arquo@users.noreply.github.com>
2023-02-18 23:07:14 -0500

    Update stockPriceAnalysisTool.py

--- stockPriceAnalysisTool.py
+++ stockPriceAnalysisTool.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
-
 import functionTool as ft
 from alpha_vantage.timeseries import TimeSeries
 
 api = "B9BL0WND53Q9UY0H"
-ts = TimeSeries(api, output_format="pandas")
+api2 = "DOBGJDVX5NOEA98X"
+ts = TimeSeries(api2, output_format="pandas")
 output_folder = r"C:\Users\chenr\OneDrive\Desktop\Pythons\Output"
 
 #class, string date, string date, "monthly"/"weekly"
+#monthly data avaliable from 
 class security_data():
     def __init__(self, symbol, start, end, freq):
         self.symbol = symbol
@@ -17,11 +18,11 @@ class security_data():
         self.freq = freq
 
         if(freq == "weekly"):
-            raw_data, raw_info = ts.get_weekly(self.symbol)
-            self.data = ft.modify_df(raw_data, start, end)
+            raw_data, raw_info = ts.get_weekly_adjusted(self.symbol)
+            self.price_data = ft.modify_df(raw_data, start, end)
         elif(freq == "monthly"):
-            raw_data, raw_info = ts.get_monthly(self.symbol)
-            self.data = ft.modify_df(raw_data, start, end)
+            raw_data, raw_info = ts.get_monthly_adjusted(self.symbol)
+            self.price_data = ft.modify_df(raw_data, start, end)
         else:
             print("Error")
     #
@@ -30,30 +31,35 @@ class security_data():
     
     #get percentage change table on prices, remove the first row
     def get_return_data(self):
-        df = pd.DataFrame(self.data)
-        df_price = df.loc[:,"Open":"Close"]
-        print(df_price)
-        df_volume = df.loc[:,"Volume"]
-        df_return = df_price.pct_change()
-        df_merged = pd.merge(df_return, df_volume, on = "date")
-        return df_merged.iloc[1:,:]
+        return ft.get_return_data(self.price_data)
     
     #create pirce date csv file in the given folder
     def price_to_csv(self, output_folder):
-        if(self.freq == "weekly"):
-            ft.wkly_to_csv(output_folder, self.data, self.symbol, self.start, self.end)
-        elif(self.freq == "monthly"):
-            ft.mly_to_csv(output_folder, self.data, self.symbol, self.start, self.end)
+        ft.df_to_csv(output_folder, self.price_data, self.symbol, self.start, self.end, self.freq)
+    
+    #Using adjusted price
+    def period_statistics(self):
+        annualized_return= ft.find_annual_return(self.price_data, self.freq)
+        annualized_std = ft.find_annual_vol(self.price_data, self.freq)
+        return [annualized_return, annualized_std]
+    
+
+
+
+MSFT1 = security_data("MSFT", "2000-12-31","2023-01-31", "weekly")
+print(MSFT1.price_data)
+print(MSFT1.get_return_data())
+print(MSFT1.period_statistics())
+
+
+
+
+
+
+
+
+
+   
         
-MSFT = security_data("MSFT", "1900-01-01","2023-02-13", "monthly")
-
-
-#print(MSFT.data[["Open","High","Low","Close"]])
-#print(MSFT.data.loc[:,'Open':'Close'])
-
-print(MSFT.get_return_data())
-MSFT.price_to_csv(output_folder)
-
-
         
         
